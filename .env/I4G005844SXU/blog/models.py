0c22994c2abacde07c90b0d2778a09ae8552fb50
Adeyemi Adekanye <sundayadeyemi8@gmail.com>
2022-06-17 22:51:24 +0100

    django project

---
+++ .env/I4G005844SXU/blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+User=get_user_model()
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    published_date = models.DateTimeField(auto_now= True)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
